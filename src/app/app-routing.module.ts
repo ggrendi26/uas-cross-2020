27130ef849c681b7f5e6d8ea36a59c19d00d267c
ggrendi26 <sakrailink@gmail.com>
2020-12-17 23:56:40 +0700

    camera dan galeri satu button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  // },
   {
     path: '',
     redirectTo: 'login',
@@ -19,26 +15,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
-  // {
-  //   path: 'add-friend',
-  //   loadChildren: () => import('./pages/add-friend/add-friend.module').then( m => m.AddFriendPageModule)
-  // },
-  // {
-  //   path: 'home-map',
-  //   loadChildren: () => import('./pages/home-map/home-map.module').then( m => m.HomeMapPageModule)
-  // },
-  // {
-  //   path: 'show-friend',
-  //   loadChildren: () => import('./pages/show-friend/show-friend.module').then( m => m.ShowFriendPageModule)
-  // },
   {
     path: 'pages',
     loadChildren: () => import('./pages/pages.module').then( m => m.PagesPageModule)
   },
-  // {
-  //   path: 'profile-user',
-  //   loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
-  // }
 ];
 
 @NgModule({
